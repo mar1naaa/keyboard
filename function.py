de404c1fd5c61aadec141a1264c1c65bc4a985f0
marina_zhinzikova <mazh607@gmail.com>
2024-12-09 23:41:33 +0300

    лаба 4+

--- function.py
+++ function.py
@@ -315,6 +315,9 @@ def proucent_penalty(qwerty, vyzov):
     return pr
 
 def vyvod_gistogramma(layout1, layout2, layout3, layout4, layout5, layout6):
+    '''
+        Выводит графики количества нажатий и штрафов
+    '''
     plt.figure(figsize=(10, 6))
     fingers = ['Мизинец (левая)', 'Безымянный (левая)', 'Средний (левая)',
                'Указательный (левая)', 'Большой (левая)',
@@ -341,7 +344,7 @@ def vyvod_gistogramma(layout1, layout2, layout3, layout4, layout5, layout6):
                  alpha=1.0)
     plt.ylabel('Пальцы')
     plt.xlabel('Количество нажатий')
-    plt.title('Сравнение нагрузок на пальцы в различных раскладках')
+    plt.title('Сравнение нагрузок на пальцы в различных раскладках, файл 25мб')
     plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x/1000)}k'))
     plt.yticks(index, fingers)
     plt.legend()
@@ -366,11 +369,94 @@ def vyvod_gistogramma(layout1, layout2, layout3, layout4, layout5, layout6):
     plt.yticks(index, fingers)
     plt.ylabel('Пальцы')
     plt.xlabel('Штрафы (в тысячах)')
-    plt.title('Штрафы на пальцы в раскладках ЙЦУКЕН и ВЫЗОВ')
+    plt.title('Штрафы на пальцы в раскладках ЙЦУКЕН и ВЫЗОВ, файл 25мб')
 
-    # Форматируем значения по оси X
     plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x/1000)}k'))
 
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+
+def get_hand_and_finger(char, fingers_dict):
+    """Определяет руку и палец, которым нажимается символ"""
+    for finger, keys in fingers_dict.items():
+        if char.lower() in keys:
+            return finger.split("f")[0]
+    return None
+
+def get_hand_and_finger_1(char, fingers_dict):
+    """Определяет руку и палец, которым нажимается символ"""
+    for finger, keys in fingers_dict.items():
+        if char.lower() in keys:
+            return finger.split("f")[1]
+    return None
+
+
+def count_digrams_with_layout(filename, fingers_dict):
+    """Считает количество диграмм для выбранной раскладки"""
+    left_hand_digrams = 0
+    right_hand_digrams = 0
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            digram = line.strip()
+            if len(digram) == 2:
+                first, second = digram[0], digram[1]
+                first_hand = get_hand_and_finger(first, fingers_dict)
+                second_hand = get_hand_and_finger(second, fingers_dict)
+                first_hand_1 = get_hand_and_finger_1(first, fingers_dict)
+                second_hand_1 = get_hand_and_finger_1(second, fingers_dict)
+
+                if first_hand == "l" and second_hand == "l":
+                    if (first_hand_1 == "i5" and second_hand_1 == 'i4') or (first_hand_1 == "i5" and second_hand_1 == 'i3')\
+                            or (first_hand_1 == "i5" and second_hand_1 == 'i2') or (first_hand_1 == "i5" and second_hand_1 == 'i1'):
+                        left_hand_digrams += 1
+                    elif (first_hand_1 == "i4" and second_hand_1 == 'i3') or (first_hand_1 == "i4" and second_hand_1 == 'i2')\
+                            or (first_hand_1 == "i4" and second_hand_1 == 'i1'):
+                        left_hand_digrams += 1
+                    elif (first_hand_1 == "i3" and second_hand_1 == 'i2') or (first_hand_1 == "fi3" and second_hand_1 == 'i1'):
+                        left_hand_digrams += 1
+                    elif first_hand_1 == "i2" and second_hand_1 == 'i1':
+                        left_hand_digrams += 1
+                elif first_hand == "r" and second_hand == "r":
+                    if (first_hand_1 == "i5" and second_hand_1 == 'i4') or (
+                            first_hand_1 == "i5" and second_hand_1 == 'i3') \
+                            or (first_hand_1 == "i5" and second_hand_1 == 'i2') or (first_hand_1 == "i5" and second_hand_1 == 'i1'):
+                        right_hand_digrams += 1
+                    elif (first_hand_1 == "i4" and second_hand_1 == 'i3') or (first_hand_1 == "i4" and second_hand_1 == 'i2')\
+                            or (first_hand_1 == "i4" and second_hand_1 == 'i1'):
+                        right_hand_digrams += 1
+                    elif (first_hand_1 == "i3" and second_hand_1 == 'i2') or (first_hand_1 == "i3" and second_hand_1 == 'i1'):
+                        right_hand_digrams += 1
+                    elif first_hand_1 == "i2" and second_hand_1 == 'i1':
+                        right_hand_digrams += 1
+
+    return left_hand_digrams, right_hand_digrams
+
+def vyvod_gistogramma3(layout7, layout8):
+    """
+        вывод в виде графика для диграмм, нажатых удобным перебором
+    """
+    plt.figure(figsize=(10, 6))
+    rasklads = ['ЙЦУКЕН','ВЫЗОВ','ДИКТОР','СКОРОПИСЬ']
+    color1 = '#ff0033'
+    color2 = '#0000ff'
+
+    index = np.arange(len(rasklads))
+    bar_width = 0.2
+
+    for i in range(len(rasklads)):
+        plt.barh(index[i] - bar_width / 2, layout7[i], bar_width, label='Левая рука' if i == 0 else "", color=color1,
+                 alpha=0.7)
+        plt.barh(index[i] + bar_width / 2, layout8[i], bar_width, label='Правая рука' if i == 0 else "", color=color2,
+                 alpha=1.0)
+
+    plt.yticks(index, rasklads)
+    plt.xlabel('Количество диграмм')
+    plt.ylabel('Раскладки')
+    plt.title('Диграммы, нажатые удобным перебором, в  различных раскладках, файл digrams')
     plt.legend()
     plt.tight_layout()
     plt.show()
--- main.py
+++ main.py
@@ -26,11 +26,13 @@ from function import count_finger_load_dictor, \
     count_finger_load_qwerty, count_finger_load_scoropis, \
     count_finger_load_vyzov, load_hand_left, \
     load_hand_right, vyvod_gistogramma, count_load_penalty_qwerty, \
-    count_load_penalty_vyzov, proucent_penalty
-
+    count_load_penalty_vyzov, proucent_penalty, get_hand_and_finger, get_hand_and_finger_1, \
+    count_digrams_with_layout, vyvod_gistogramma3
+from dict_finger import keyboard_finger_dictor, keyboard_finger_qwerty, keyboard_finger_scoropis, keyboard_finger_vyzov
 if __name__ == "__main__":
-    with open('full_text.txt', "r", encoding='utf-8') as f:
+    with open('digrams.txt', "r", encoding='utf-8') as f:
         text = f.read()
+    """"
     qwerty_finger_load = count_finger_load_qwerty(text)
     vyzov_finger_load = count_finger_load_vyzov(text)
     dictor_finger_load = count_finger_load_dictor(text)
@@ -82,5 +84,29 @@ if __name__ == "__main__":
     vyvod_gistogramma(qwerty_finger_load,
                       vyzov_finger_load, dictor_finger_load,
                       scoropis_finger_load, penalty_qwerty, penalty_vyzov)
+    """
+    input_file = 'digrams.txt'
+    left_qwerty, right_qwerty = count_digrams_with_layout(input_file, keyboard_finger_qwerty)
+    left_vyzov, right_vyzov = count_digrams_with_layout(input_file, keyboard_finger_vyzov)
+    left_dictor, right_dictor = count_digrams_with_layout(input_file, keyboard_finger_dictor)
+    left_scoropis, right_scoropis = count_digrams_with_layout(input_file, keyboard_finger_scoropis)
+    lv = [left_qwerty, left_vyzov, left_dictor, left_scoropis]
+    rv = [right_qwerty, right_vyzov, right_dictor, right_scoropis]
+    vyvod_gistogramma3(lv, rv)
+    print("ЙЦУКЕН:")
+    print(f"Диграммы, нажатые левой рукой: {left_qwerty}")
+    print(f"Диграммы, нажатые правой рукой: {right_qwerty}")
+
+    print("\nВЫЗОВ:")
+    print(f"Диграммы, нажатые левой рукой: {left_vyzov}")
+    print(f"Диграммы, нажатые правой рукой: {right_vyzov}")
+    
+    print("\nДИКТОР:")
+    print(f"Диграммы, нажатые левой рукой: {left_dictor}")
+    print(f"Диграммы, нажатые правой рукой: {right_dictor}")
+
+    print("\nСКОРОПИСЬ:")
+    print(f"Диграммы, нажатые левой рукой: {left_scoropis}")
+    print(f"Диграммы, нажатые правой рукой: {right_scoropis}")
 
 
